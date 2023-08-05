6d4549307806adc9d8f1302ff77d955f53a5d038
archanak55201 <55021100+archanak55201@users.noreply.github.com>
2023-08-05 21:41:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,113 @@
+const apiKey = "7afea6734fcf093e0b4d5cff26721863";
+console.log("script is connected");
+
+const MainContent = document.getElementsByClassName("main-content")[0];
+const set = new Set();
+const button = document.getElementById("search");
+button.addEventListener("submit",function(event){
+    event.preventDefault();
+    const cityName = document.getElementById("cityName");
+    const city = cityName.value;
+    
+    set.add(city);
+    console.log(set);
+    weatherReport();
+
+})
+
+async function weatherReport(){
+    const weatherData =[];
+    for(const city of set){
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+        const response = await fetch(apiUrl);
+        const result = await response.json();
+        console.log(result);
+        const obj ={
+            name:`${result.name}`,//
+            type:`${result.weather[0].main}`,//
+            temp:`${result.main.temp}`,//
+            tempmin:`${result.main.temp_min}`,//
+            tempmax:`${result.main.temp_max}`,//
+            humidity:`${result.main.humidity}`,//
+            pressure:`${result.main.pressure}`,//
+            winds:`${result.wind.speed}`,//
+            deg:`${result.wind.deg}`,//
+            cloudp:`${result.clouds.all}`,//
+            code:`${result.sys.country}`,//
+            sunrise:`${result.sys.sunrise}`,
+            sunset:`${result.sys.sunset}`
+        }
+        weatherData.push(obj);
+    }
+
+    weatherData.sort((a,b) => a.temp-b.temp);
+    console.log(weatherData);
+
+    MainContent.innerHTML="";
+    weatherData.forEach((value)=>{
+
+        const weathertype = getWeatherImg(value.type);
+        console.log(weathertype);
+
+    const gridItem = document.createElement("div");
+    gridItem.innerHTML = `
+            <div class="grid-content">
+                            
+                <div style=" display: flex;flex-direction: row;justify-content: space-between;">
+                    <div  style=" display: flex;flex-direction: column; padding-bottom: 5px;">
+                    <span><i class="fa-solid fa-gauge-simple-high" style="color: #e6ebf4;padding-right: 7px;"></i>${value.winds}km/h</span> 
+                    <span><i class="fa-regular fa-compass" style="color: #eaecf0;padding-right: 7px;"></i>${value.deg}</span> 
+                    <span><i class="fa-solid fa-droplet" style="color: #f0f2f5;padding-right: 7px;"></i>${value.cloudp}</span> 
+                        
+                        
+                    </div>
+                    <img src="${weathertype}" style="height: 20vh;position: absolute;padding-left: 15vw;">
+                </div>
+                
+                <div class="details">
+                    <div style="font-size: 65px;">
+                
+                        ${value.temp}<sup style="font-size: 20px;position: absolute;">o</sup>
+            
+                    </div>
+                    <div class="high-low">
+                        <div>H:${value.tempmin}<sup>o</sup></div>
+                        <div>L:${value.tempmax}<sup>o</sup></div>
+                    </div>
+                    <div class="high-low">
+                        <div>Humidity:${value.humidity}</div>
+                        <div>pressure:${value.pressure}</div>
+                    </div>
+                    <div class="cot-type">
+                        <div>${value.name},${value.code}</div>   
+                        <div class="dayType">${value.type}</div>   
+                    </div>
+                </div>
+
+            </div>
+    `;
+
+    MainContent.appendChild(gridItem);
+    gridItem.className="grid-item";
+    });
+}
+
+
+function getWeatherImg(type){
+    if(type==="Fog"||type==="Haze"||type==="Smoke"||type==="Clouds"||type==="Mist"||type==="Dust"||type==="Sand"){
+        return "wind.svg"
+    }
+    else if(type==="Snow"||type==="Squall"){
+        return "srain.svg"
+    }
+    else if(type==="Drizzle"||type==="Rain"){
+        return "mrain.svg";
+    }
+
+    else if(type==="Thunderstorm"||type==="Tornado"){
+        return "Tornado.svg";
+    }else{
+        return "sun.svg";
+    }
+}
+
